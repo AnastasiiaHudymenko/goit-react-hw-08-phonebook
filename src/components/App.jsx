de7710c46df10898c2af5bbf06cb4735058d824a
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-03-29 14:17:21 +0300

    add pages Home,Login,Register and Contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
+import { HomePage } from '../pages/Home';
+import { RegisterPage } from '../pages/Register';
+import { LoginPage } from '../pages/Login';
+import logo from '../image/contacts.png';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <header>
+        <img width={60} height={60} src={logo} alt="" />
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/register">Register</NavLink>
+          <NavLink to="/login">Login</NavLink>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
     </div>
   );
 };
